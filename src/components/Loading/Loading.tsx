f359ec4cbfe95461e1ebed8cd2f4f27ea5ee3f5a
NiRenn <kigor768@gmail.com>
2026-04-22 16:16:04 +0300

    check2

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -38,7 +38,7 @@ function Loading() {
     let navigated = false;
 
     // console.log(tg.initData);
-    console.log(tg.id)
+    // console.log(tg.id)
 
     const go = (to: string) => {
       if (navigated) return;
