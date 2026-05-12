ddbe24661cbb5a4f4502103577e1d732e6bc1ae1
NiRenn <kigor768@gmail.com>
2026-05-12 10:52:55 +0300

    link riil

--- src/components/Onboarding/Onboarding.tsx
+++ src/components/Onboarding/Onboarding.tsx
@@ -14,7 +14,7 @@ import { INFO_IMAGES, FINAL_IMAGES } from "../../config/preloadAssets";
 import { preloadImageSrcs } from "../../utils/preload";
 import { useAppStore } from "../../store/appStore";
 
-const TARIFF_URL = "https://moskva.mts.ru/personal/mobilnaya-svyaz/tarifi/vse-tarifi/riil?utm_source=mrk_sp&utm_medium=tg&utm_campaign=msc_mts_riil_q2_26&utm_term=test_vzroslay_zhizn";
+const TARIFF_URL = "https://moskva.mts.ru/personal/mobilnaya-svyaz/uslugi/mobilnaya-svyaz/riil-tarif?utm_source=mrk_sp&utm_medium=banner&utm_campaign=mcs_riil_q2_26&utm_term=testvzroslaya";
 
 function Onboarding() {
   const navigate = useNavigate();
